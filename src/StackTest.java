41c1a475c320e628e66463a8b6bf7e4f51df916e
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-03 20:01:24 +0300

    stack array

---
+++ src/StackTest.java
@@ -0,0 +1,21 @@
+public class StackTest {
+    public static void main(String[] args)
+    {
+        StackX thestack = new StackX(10);
+        thestack.push(20);
+        thestack.push(40);
+        thestack.push(60);
+        thestack.push(80);
+        thestack.push(90);
+
+        thestack.push(200);
+        thestack.push(300);
+        thestack.push(45);
+        thestack.push(25);
+        thestack.push(35);
+
+        for(int i=0; i<10; i++){
+            System.out.println(thestack.stackArray[i]);
+        }
+    }
+}
